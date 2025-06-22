Fixed rotation and resampling looks good.

src/itemcloud/item_cloud.py
@@ -1,6 +1,7 @@
 import warnings
 import numpy as np
-from itemcloud.containers.base.image_item import ImageItem
+from PIL import Image
+from itemcloud.containers.base.image_item import ImageItem, set_resize_resampling, set_rotate_resampling
 from itemcloud.logger.base_logger import BaseLogger
 from itemcloud.size import (Size, ResizeType)
 from itemcloud.util.parsers import (parse_to_float, parse_to_int)
@@ -82,6 +83,16 @@ class ItemCloud(object):
         A pixel is considered transparent 
         if its alpha value is <= this percent of 255. 
         0(fully-transparent) - (partly transparent) - 100(fully-opaque)
+
+    resize_resampling: Image.Resampling
+        Resampling to use when resizing or rotating image.
+        0 = NEAREST, 1 = LANCZOS, 2 = BILINEAR, 3 = BICUBIC, 4 = BOX, 5 = HAMMING
+        https://pillow.readthedocs.io/en/stable/handbook/concepts.html#concept-filters
+
+    rotate_resampling: Image.Resampling
+        Resampling to use when resizing or rotating image.
+        0 = NEAREST, 1 = LANCZOS, 2 = BILINEAR, 3 = BICUBIC, 4 = BOX, 5 = HAMMING
+        https://pillow.readthedocs.io/en/stable/handbook/concepts.html#concept-filters
     
     mode : string (default=objectcloud_defaults.DEFAULT_MODE)
         Transparent background will be generated when mode is "RGBA" and
@@ -103,6 +114,8 @@ class ItemCloud(object):
         contour_color: str | None = None,
         margin: int | None = None,
         opacity: int | None = None,
+        resize_resampling: Image.Resampling | None = None,
+        rotate_resampling: Image.Resampling | None = None,
         mode: str | None = None,
         name: str | None = None,
         total_threads: int | None = None,
@@ -125,12 +138,17 @@ class ItemCloud(object):
 
         self._margin = margin if margin is not None else parse_to_int(item_cloud_defaults.DEFAULT_MARGIN)
         self._opacity = opacity if opacity is not None else parse_to_int(item_cloud_defaults.DEFAULT_OPACITY)
+        self._resize_resampling = resize_resampling if resize_resampling is not None else Image.Resampling(parse_to_int(item_cloud_defaults.DEFAULT_RESAMPLING))
+        self._rotate_resampling = rotate_resampling if rotate_resampling is not None else Image.Resampling(parse_to_int(item_cloud_defaults.DEFAULT_RESAMPLING))
+
         self._mode = mode if mode is not None else item_cloud_defaults.DEFAULT_MODE
         self._name = name if name is not None else 'itemcloud'
         self._total_threads = total_threads if total_threads is not None else parse_to_int(item_cloud_defaults.DEFAULT_TOTAL_THREADS)
         self._search_pattern = search_pattern if search_pattern is not None else SearchPattern[item_cloud_defaults.DEFAULT_SEARCH_PATTERN]
         self.layout_: Layout | None = None
         set_opacity_percentage(self._opacity)
+        set_resize_resampling(self._resize_resampling)
+        set_rotate_resampling(self._rotate_resampling)
 
     @property
     def mask(self) -> np.ndarray | None:
@@ -329,6 +347,8 @@ class ItemCloud(object):
             layout.scale,
             layout.margin,
             layout.opacity,
+            layout.resize_resampling,
+            layout.rotate_resampling,
             layout.name + '.maximized',
             self._total_threads,
             latency_str,
@@ -481,6 +501,8 @@ class ItemCloud(object):
             self._scale,
             self._margin,
             self._opacity,
+            self._resize_resampling,
+            self._rotate_resampling,
             self._name + '.layout',
             self._total_threads,
             latency_str,